549472aa531e1828924a6f622014bb65d5a76437
jeny3g <jeny3g@icloud.com>
2023-04-14 14:36:09 -0300

    Merge remote-tracking branch 'refs/remotes/origin/master'

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +24,9 @@ class ViewController: UIViewController {
     @IBAction func handleRollButtonPressed(_ sender: UIButton) {
         let diceImages = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix")];
         
-        diceImageView1.image = diceImages[leftDiceNumber]
-        diceImageView2.image = diceImages[rightDiceNumber]
-        
-        leftDiceNumber = leftDiceNumber + 1;
-        rightDiceNumber = rightDiceNumber - 1;
+        diceImageView1.image = diceImages[Int.random(in: 0...5)]
+        diceImageView2.image = diceImages[Int.random(in: 0...5)]
+
     }
     
 }
